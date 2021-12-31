8096ab3db217725940db9f0e3d64c2683b752639
Abhishek Agarwal <abhishek.ag00774@gmail.com>
2021-12-31 17:52:21 +0530

    Done with Blink

---
+++ RoboManipal/Worksheet-2/Blink/Blink 2sec.ino
@@ -0,0 +1,14 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000); // Wait for 2000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
